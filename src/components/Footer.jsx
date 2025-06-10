43d3ecefff9b40759824a622568ab1e4fb96df25
metatony <ibegbulam.ua@gmail.com>
2025-06-10 21:51:38 +0100

    style: enhance footer and collection banner for improved readability and layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex flex-col space-y-12 bg-[#f5f4f4] pt-[76px] px-5 text-left text-xl '> 
+    <div className='flex flex-col lg:grid grid-cols-5 gap-1 font-light lg:text-sm space-y-12 bg-[#f5f4f4] pt-[76px] px-5 text-left text-xl '> 
 
       <div className='space-y-1 w-full'>
-        <p className='text-[#565656] mb-5'>CONTACT US</p>
+        <p className='text-[#565656] mb-5 font-medium'>CONTACT US</p>
         <p>+1 (844) 326-6000</p>
         <p>Email Us</p>
         <p>Mon-Fri 9am-3pm PT</p>
       </div>
 
       <div className='space-y-1 w-full'>
-        <p className='text-[#565656] mb-5'>CUSTOMERS</p>
+        <p className='text-[#565656] mb-5 font-medium'>CUSTOMERS</p>
         <p>Start a Return</p>
         <p>Return Policy</p>
         <p>FAQ</p>
@@ -22,7 +22,7 @@ function Footer() {
 
 
       <div className='space-y-1 w-full'>
-        <p className='text-[#565656] mb-5'>COMPANY</p>
+        <p className='text-[#565656] mb-5 font-medium'>COMPANY</p>
         <p>About Us</p>
         <p>Sustainability</p>
         <p>Discover Revive</p>
@@ -32,16 +32,16 @@ function Footer() {
       </div>
 
 
-      <div className='space-y-4'>
+      <div className='space-y-4 col-span-2'>
         <p className='text-xl font-medium tracking-wider'>Get the latest new from us</p>
         <p className=''>By signing up, you agree to our <span className='underline'>Privacy Policy</span>  and  <span className='underline'>Terms of Service.</span> </p>
         <button className='bg-black text-white text-lg w-36 text-center py-[8px]'>Subscribe</button>
       </div>
 
-      <div className='text-center w-full pb-10'>
+      {/* <div className='text-center w-full pb-10'> */}
         {/* <p>&copy;CEIN </p> */}
-        <p>&copy; Metatony </p>
-      </div>
+        {/* <p>&copy; Metatony </p>
+      </div> */}
 
 
 
--- src/pages/landing page/collection banner/CollectionBanner.jsx
+++ src/pages/landing page/collection banner/CollectionBanner.jsx
@@ -15,9 +15,9 @@ function CollectionBanner() {
           </div>
 
           <div className='flex text-2xl font-light flex-col justify-center items-center text-center mt-5 px-5  bg-[#f5f4f4] pt-[60px] '>
-              <p className='mb-7 tracking-wider font-medium'>The Art of Fewer, <br /> Better Choices</p>
+              <p className='mb-7 tracking-wider font-medium'>The Art of Fewer, <br className='lg:hidden'/> Better Choices</p>
 
-              <p className='text-xl  font-light w-full mb-[150px]'>Opting for quality over quantity means selecting timeless, durable, and responsibly made items. This approach simplifies our lives and fosters a deeper appreciation for our surroundings. Emphasizing longevity and responsible production resonates with a more sustainable and mindful lifestyle.</p>
+              <p className='text-xl font-light w-full mb-[150px] lg:w-[720px] lg:text-lg'>Opting for quality over quantity means selecting timeless, durable, and responsibly made items. This approach simplifies our lives and fosters a deeper appreciation for our surroundings. Emphasizing longevity and responsible production resonates with a more sustainable and mindful lifestyle.</p>
           </div> 
 
 
